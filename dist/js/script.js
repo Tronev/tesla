b442d68e8df4219e3c5d940b2dce9846723cd131
Tronev_Ivan <74rus_86@mail.ru>
2020-06-28 11:36:43 +0600

    load files in dist

---
+++ dist/js/script.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.slider').slick({
+        arrows:false,
+        fade:true,
+        autoplay:3000,
+        dots:true,
+        
+    });
+
+    $('.header-btn').on('click', function (){
+        $('.menu').addClass('active');
+    });
+    
+    $('.close-btn').on('click', function (){
+        $('.menu').removeClass('active');
+    });
+    
+    // модальные окна
+
+    $('[data-modal=consultation]').on('click',function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click',function(){
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+});  
